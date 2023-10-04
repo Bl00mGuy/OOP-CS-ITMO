91384c200c4398e6372ed56db2c42a3a7e451925
Said Gadzhiev <good.day.just.chill@gmail.com>
2023-10-04 17:54:42 +0300

    fix: fixed Route

--- src/Lab1/Environment/Entities/Environment.cs
+++ src/Lab1/Environment/Entities/Environment.cs
@@ -1,47 +1,118 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Itmo.ObjectOrientedProgramming.Lab1.Environment.Models;
+using Itmo.ObjectOrientedProgramming.Lab1.Environment.Services;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities;
 
 namespace Itmo.ObjectOrientedProgramming.Lab1.Environment.Entities;
 
-public abstract class Environment : ShipEntersSphere
+public abstract class Environment
 {
-    private readonly Collection<string> _requiredEngineNames = new Collection<string>();
-    private readonly Collection<string> _requiredJumpEngineNames = new Collection<string>(); // Добавление коллекции для требуемых прыжковых двигателей
+    private readonly Collection<string> _requiredEngineNames = new();
+    private readonly Collection<string> _requiredJumpEngineNames = new();
 
-    public ReadOnlyCollection<string> RequiredEngineNames => _requiredEngineNames.AsReadOnly();
-    public ReadOnlyCollection<string> RequiredJumpEngineNames => _requiredJumpEngineNames.AsReadOnly(); // Добавление свойства для RequiredJumpEngineNames
+    public double LengthOfEnvironment { get; protected init; }
 
-    public Dictionary<string, int> Obstacles { get; } = new Dictionary<string, int>();
+    private ReadOnlyCollection<string> RequiredEngineNames => _requiredEngineNames.AsReadOnly();
+    private ReadOnlyCollection<string> RequiredJumpEngineNames => _requiredJumpEngineNames.AsReadOnly();
+    private Dictionary<string, int> Obstacles { get; } = new();
 
-    public double LengthOfEnvironment { get; protected set; }
+    protected static void ShipEnterSphere(Spaceship spaceship, Environment segment)
+    {
+        if (spaceship.Engine != null)
+        {
+            switch (segment)
+            {
+                case NormalSpace:
+                    if (segment.RequiredEngineNames.Contains(spaceship.Engine.EngineName))
+                    {
+                        if (segment.Obstacles.TryGetValue("SmallAsteroid", out int smallAsteroidCount))
+                        {
+                            for (int i = 0; i < smallAsteroidCount; i++)
+                            {
+                                var smallAsteroid = new SmallAsteroid();
+                                smallAsteroid.ObstacleHit(spaceship);
+                            }
+                        }
 
-    public void AddRequiredEngineName(string engineName)
+                        if (segment.Obstacles.TryGetValue("Meteorite", out int meteoriteCount))
+                        {
+                            for (int i = 0; i < meteoriteCount; i++)
+                            {
+                                var meteorite = new Meteorite();
+                                meteorite.ObstacleHit(spaceship);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Spaceship doesn't have a required engine for Normal Space!");
+                    }
+
+                    break;
+
+                case HighDensityFog:
+                    if (spaceship.JumpEngine != null)
+                    {
+                        if (segment.RequiredEngineNames.Contains(spaceship.Engine.EngineName) && segment.RequiredJumpEngineNames.Contains(spaceship.JumpEngine.EngineName))
+                        {
+                            if (segment.LengthOfEnvironment > spaceship.JumpEngine.MaxJumpLength)
+                            {
+                                throw new ArgumentException("Spaceship doesn't have a required jump engine for High-Density Fog!");
+                            }
+
+                            if (segment.Obstacles.TryGetValue("AntimatterFlare", out int antimatterFlaresCount))
+                            {
+                                for (int i = 0; i < antimatterFlaresCount; i++)
+                                {
+                                    var antimatterFlare = new AntimatterFlares();
+                                    antimatterFlare.ObstacleHit(spaceship);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Spaceship doesn't have a required engine for High-Density Fog!");
+                    }
+
+                    break;
+
+                case NitrineParticleFog:
+                    if (segment.RequiredEngineNames.Contains(spaceship.Engine.EngineName))
+                    {
+                        if (segment.Obstacles.TryGetValue("SpaceWhale", out int spaceWhalesCount))
+                        {
+                            for (int i = 0; i < spaceWhalesCount; i++)
+                            {
+                                var spaceWhales = new SpaceWhales();
+                                spaceWhales.ObstacleHit(spaceship);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Spaceship doesn't have a required engine for Nitrine-Particle Fog!");
+                    }
+
+                    break;
+            }
+        }
+    }
+
+    protected void AddRequiredEngineName(string engineName)
     {
         _requiredEngineNames.Add(engineName);
     }
 
-    public void RemoveRequiredEngineName(string engineName)
-    {
-        _requiredEngineNames.Remove(engineName);
-    }
-
-    public void AddRequiredJumpEngineName(string jumpEngineName) // Метод для добавления требуемых прыжковых двигателей
+    protected void AddRequiredJumpEngineName(string jumpEngineName)
     {
         _requiredJumpEngineNames.Add(jumpEngineName);
     }
 
-    public void RemoveRequiredJumpEngineName(string jumpEngineName) // Метод для удаления требуемых прыжковых двигателей
-    {
-        _requiredJumpEngineNames.Remove(jumpEngineName);
-    }
-
-    public void AddObstacle(string obstacle, int count)
+    protected void AddObstacle(string obstacle, int count)
     {
         Obstacles[obstacle] = count;
-    }
-
-    public void RemoveObstacle(string obstacle)
-    {
-        Obstacles.Remove(obstacle);
     }
 }
--- tests/Lab1.Tests/Test.cs
+++ tests/Lab1.Tests/Test.cs
@@ -10,54 +10,30 @@ namespace Itmo.ObjectOrientedProgramming.Lab1.Tests;
 public class Test
 {
     [Theory]
-    [InlineData(10000, 10000, 0, 10, 200, 5)] // Прогулочный челнок без прыжковых двигателей
-    public void TestRouteForSpaceships(double initialFuelActivePlasma, double initialFuelGravitonMatter, double fuelActivePlasmaPrice, double fuelGravitonMatterPrice, int normalSpaceLength, int highDensityFogLength)
+    [InlineData(10000, 10000, 1, 1, 200)] // Прогулочный челнок без прыжковых двигателей
+    public void TestRouteForSpaceships(double initialFuelActivePlasma, double initialFuelGravitonMatter, double fuelActivePlasmaPrice, double fuelGravitonMatterPrice, int highDensityFogLength)
     {
         var spaceship1 = new PleasureShuttleShip();
         var spaceship2 = new AvgurShip();
 
-        var normalSpaceSegment = new NormalSpace(0, 0, normalSpaceLength);
         var highDensityFogSegment = new HighDensityFog(0, highDensityFogLength);
 
-        var segments1 = new Collection<Environment.Entities.Environment> { normalSpaceSegment, highDensityFogSegment };
-        var segments2 = new Collection<Environment.Entities.Environment> { normalSpaceSegment, highDensityFogSegment };
+        var segments = new Collection<Environment.Entities.Environment> { highDensityFogSegment };
 
-        // Act
-        Action sendSpaceship1 = () =>
+        ArgumentOutOfRangeException ex1 = Assert.Throws<ArgumentOutOfRangeException>(() =>
         {
-            Route.SendSpaceshipVoyage(spaceship1, initialFuelActivePlasma, initialFuelGravitonMatter, fuelActivePlasmaPrice, fuelGravitonMatterPrice, segments1);
-        };
+            Route.SendSpaceshipVoyage(spaceship1, initialFuelActivePlasma, initialFuelGravitonMatter, fuelActivePlasmaPrice, fuelGravitonMatterPrice, segments);
+        });
 
-        Action sendSpaceship2 = () =>
+        ArgumentOutOfRangeException ex2 = Assert.Throws<ArgumentOutOfRangeException>(() =>
         {
-            Route.SendSpaceshipVoyage(spaceship2, initialFuelActivePlasma, initialFuelGravitonMatter, fuelActivePlasmaPrice, fuelGravitonMatterPrice, segments2);
-        };
+            Route.SendSpaceshipVoyage(spaceship2, initialFuelActivePlasma, initialFuelGravitonMatter, fuelActivePlasmaPrice, fuelGravitonMatterPrice, segments);
+        });
 
-        // Assert
-        if (initialFuelActivePlasma <= 0)
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(sendSpaceship1);
-            Assert.Throws<ArgumentOutOfRangeException>(sendSpaceship2);
-        }
-        else
-        {
-            if (fuelActivePlasmaPrice <= 0)
-            {
-                Assert.Throws<ArgumentException>(sendSpaceship1);
-                Assert.Throws<ArgumentException>(sendSpaceship2);
-            }
-            else
-            {
-                Assert.Throws<InvalidOperationException>(sendSpaceship1);
-                if (initialFuelGravitonMatter <= 0 || fuelGravitonMatterPrice <= 0)
-                {
-                    Assert.Throws<ArgumentException>(sendSpaceship2);
-                }
-                else
-                {
-                    Assert.Throws<InvalidOperationException>(sendSpaceship2);
-                }
-            }
-        }
+        Assert.Contains("initialFuelActivePlasma", ex1.Message, StringComparison.OrdinalIgnoreCase);
+        Assert.Contains("initialFuelGravitonMatter", ex1.Message, StringComparison.OrdinalIgnoreCase);
+
+        Assert.Contains("initialFuelActivePlasma", ex2.Message, StringComparison.OrdinalIgnoreCase);
+        Assert.Contains("initialFuelGravitonMatter", ex2.Message, StringComparison.OrdinalIgnoreCase);
     }
 }
