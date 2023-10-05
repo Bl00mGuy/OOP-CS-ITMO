ed9876bbbb54f20e1d10cb4b71d26556944a4bcb
Said Gadzhiev <good.day.just.chill@gmail.com>
2023-10-05 11:45:54 +0300

    feat: created Sixth Test

---
+++ tests/Lab1.Tests/SixthTest.cs
@@ -0,0 +1,40 @@
+using System.Collections.ObjectModel;
+using Itmo.ObjectOrientedProgramming.Lab1.Environment.Entities;
+using Itmo.ObjectOrientedProgramming.Lab1.Environment.Models;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models;
+using Xunit;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Tests;
+
+public class SixthTest
+{
+    [Theory]
+    [InlineData(10000, 10000, 1, 1, 200)]
+    public void TestRouteForSpaceships(double initialFuelActivePlasma, double initialFuelGravitonMatter, double fuelActivePlasmaPrice, double fuelGravitonMatterPrice, int highDensityFogLength)
+    {
+        // Arrange
+        var spaceship1 = new PleasureShuttleShip();
+        var spaceship2 = new VaklasShip(false);
+        var nitrineParticleFog = new NitrineParticleFog(0, highDensityFogLength);
+
+        var segments = new Collection<Environment.Entities.Environment> { nitrineParticleFog };
+
+        string expectedOutput1 = "Spaceship doesn't have a required engine!";
+        string expectedOutput2 = "The spacecraft has successfully complete voyage";
+
+        // Act
+        string SendSpaceship1()
+        {
+            return Route.SendSpaceshipVoyage(spaceship1, initialFuelActivePlasma, initialFuelGravitonMatter, fuelActivePlasmaPrice, fuelGravitonMatterPrice, segments);
+        }
+
+        string SendSpaceship2()
+        {
+            return Route.SendSpaceshipVoyage(spaceship2, initialFuelActivePlasma, initialFuelGravitonMatter, fuelActivePlasmaPrice, fuelGravitonMatterPrice, segments);
+        }
+
+        // Assert
+        Assert.Equal(expectedOutput1, SendSpaceship1());
+        Assert.Equal(expectedOutput2, SendSpaceship2());
+    }
+}
