f43dd2d33cd4e4141a6b1ae4b4da81d22b2bf95e
Said Gadzhiev <good.day.just.chill@gmail.com>
2023-10-05 12:04:58 +0300

    fix: fixed Seventh Test

--- tests/Lab1.Tests/SeventhTest.cs
+++ tests/Lab1.Tests/SeventhTest.cs
@@ -9,7 +9,7 @@ namespace Itmo.ObjectOrientedProgramming.Lab1.Tests;
 public class SeventhTest
 {
     [Theory]
-    [InlineData(10000, 10000, 1, 1, 200, 100, 200)]
+    [InlineData(10000, 10000, 1, 1, 300, 100, 200)]
     public void TestRouteForSpaceships(double initialFuelActivePlasma, double initialFuelGravitonMatter, double fuelActivePlasmaPrice, double fuelGravitonMatterPrice, int normalSpaceLength, int highDensityFogLength, int nitrineParticleFogLength)
     {
         // Arrange
